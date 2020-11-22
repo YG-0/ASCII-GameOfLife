6cec71f88d8f2acde02a2e14fb58ff94ff43b17f
ygeyzel <ygeyzel@gmail.com>
2020-11-22 22:43:16 +0200

    format

--- src/Cell.hs
+++ src/Cell.hs
@@ -1,26 +1,25 @@
-module Cell (
-    Cell(..),
-    readCell,
-    isAlive,
-    cellNextState,
-    neighboursIndexes
-) where
+module Cell
+  ( Cell(..)
+  , readCell
+  , isAlive
+  , cellNextState
+  , neighboursIndexes
+  ) where
 
-import Data.List (delete)
+import           Data.List                      ( delete )
 
 data Cell = Dead | Alive deriving(Eq, Read)
 instance Show Cell where
-    show Alive = "#"
-    show Dead  = "-"
+  show Alive = "#"
+  show Dead  = "-"
 
 isAlive :: Cell -> Bool
-isAlive  = (== Alive)
+isAlive = (== Alive)
 
 readCell :: Char -> Maybe Cell
-readCell c
-    | c == '-'  = Just Dead
-    | c == '#'  = Just Alive
-    | otherwise = Nothing
+readCell c | c == '-'  = Just Dead
+           | c == '#'  = Just Alive
+           | otherwise = Nothing
 
 {- |
  - Returns the next state of a cell
@@ -28,19 +27,13 @@ readCell c
  - neighbours
  -}
 cellNextState :: Cell -> Int -> Cell
-cellNextState cell liveCells =
-    if isAlive cell then
-        if (liveCells == 2 || liveCells == 3)
-        then Alive
-        else Dead
-    else
-        if liveCells == 3
-        then Alive
-        else Dead
+cellNextState cell liveCells = if isAlive cell
+  then if (liveCells == 2 || liveCells == 3) then Alive else Dead
+  else if liveCells == 3 then Alive else Dead
 
 -- |Indexes of a cell's neighbours (may be out of bounds)
 neighboursIndexes :: (Int, Int) -> [(Int, Int)]
 neighboursIndexes (x, y) =
-    let indexes  = [[(x + dx, y + dy) | dx <- [-1..1]] | dy <- [-1..1]]
-    in delete (x, y) $ concat indexes
-    
+  let indexes = [ [ (x + dx, y + dy) | dx <- [-1 .. 1] ] | dy <- [-1 .. 1] ]
+  in  delete (x, y) $ concat indexes
+
--- src/Main.hs
+++ src/Main.hs
@@ -1,9 +1,11 @@
-import Board
-import System.Console.GetOpt
-import Data.Maybe (fromJust, fromMaybe)
-import System.Environment (getArgs)
-import Control.Concurrent (threadDelay)
-import System.Process (system)
+import           Board
+import           Control.Concurrent             ( threadDelay )
+import           Data.Maybe                     ( fromJust
+                                                , fromMaybe
+                                                )
+import           System.Console.GetOpt
+import           System.Environment             ( getArgs )
+import           System.Process                 ( system )
 
 
 defaultFile :: String
@@ -15,7 +17,7 @@ defaultSteps = 100
 defaultDelayMS :: Int
 defaultDelayMS = 100
 
--- |Command line arguments flags 
+-- |Command line arguments flags
 data Flag = Help
           | Input String
           | Steps Int
@@ -24,68 +26,72 @@ data Flag = Help
 
 
 options :: [OptDescr Flag]
-options = [
-             Option
-                ['h'] ["help"] (NoArg Help)
-                "Show help"
-           , Option
-                ['i'] ["input"] (ReqArg Input "FILE")
-                "Input file with board starting state"
-           , Option
-                ['s'] ["steps"] (ReqArg (Steps . read) "NUM")
-                "Number of steps"
-           , Option
-                ['d'] ["delay"] (ReqArg (Delay . read) "MS")
-                "Delay in milliseconds between steps"
-          ]
+options =
+  [ Option ['h'] ["help"] (NoArg Help) "Show help"
+  , Option ['i']
+           ["input"]
+           (ReqArg Input "FILE")
+           "Input file with board starting state"
+  , Option ['s'] ["steps"] (ReqArg (Steps . read) "NUM") "Number of steps"
+  , Option ['d']
+           ["delay"]
+           (ReqArg (Delay . read) "MS")
+           "Delay in milliseconds between steps"
+  ]
 
 
 main = do
-    argv <- getArgs
-    let (opts, _, err) = getOpt Permute options argv
-    if not (null err)
-       then ioError $ userError $ concat err ++ usageInfo "\nUsage:" options
-       else do
-           let (help, input, steps, delay) =
-                   parseOpts opts defaultFile defaultSteps defaultDelayMS
-           if help
-              then putStrLn $ usageInfo "\nUsage" options
-              else do 
-                  initBoardStr <- readFile input
-                  let initBoard = readBoard $ filter (/= ' ') initBoardStr
-                  if initBoard == Nothing
-                     then ioError $ userError "Invalid board file"
-                     else playNSteps steps delay $ fromJust initBoard
+  argv <- getArgs
+  let (opts, _, err) = getOpt Permute options argv
+  if not (null err)
+    then ioError $ userError $ concat err ++ usageInfo "\nUsage:" options
+    else do
+      let (help, input, steps, delay) =
+            parseOpts opts defaultFile defaultSteps defaultDelayMS
+      if help
+        then putStrLn $ usageInfo "\nUsage" options
+        else do
+          initBoardStr <- readFile input
+          let initBoard = readBoard $ filter (/= ' ') initBoardStr
+          if initBoard == Nothing
+            then ioError $ userError "Invalid board file"
+            else playNSteps steps delay $ fromJust initBoard
 
 
-parseOpts :: [Flag] ->          -- list of command line arguments flags
-             String ->          -- default input file name
-             Int    ->          -- default steps
-             Int    ->          -- default delay
-             (Bool, String, Int, Int) -- (help-flag, input-file-name, steps, delay)
+parseOpts
+  :: [Flag]
+  ->          -- list of command line arguments flags
+     String
+  ->          -- default input file name
+     Int
+  ->          -- default steps
+     Int
+  ->          -- default delay
+     (Bool, String, Int, Int) -- (help-flag, input-file-name, steps, delay)
 parseOpts opts dInput dSteps dDelay =
-    (
-        exist          Help
-      , firstOrDefault input dInput
-      , firstOrDefault steps dSteps
-      , firstOrDefault delay dDelay
-    )
-        where
-            input = [i | (Input i) <- opts]
-            steps = [s | (Steps s) <- opts]
-            delay = [s | (Delay s) <- opts]
-            firstOrDefault l d = if null l then d else head l
-            exist item         = item `elem` opts
+  ( exist Help
+  , firstOrDefault input dInput
+  , firstOrDefault steps dSteps
+  , firstOrDefault delay dDelay
+  )
+ where
+  input = [ i | (Input i) <- opts ]
+  steps = [ s | (Steps s) <- opts ]
+  delay = [ s | (Delay s) <- opts ]
+  firstOrDefault l d = if null l then d else head l
+  exist item = item `elem` opts
 
 
-playNSteps :: Int   -> -- steps
-              Int   -> -- delay
-              Board -> -- init board
-              IO ()
+playNSteps
+  :: Int
+  -> -- steps
+     Int
+  -> -- delay
+     Board
+  -> -- init board
+     IO ()
 playNSteps n delay board = do
-    system "clear"
-    putStrLn $ showBoard board
-    threadDelay $ delay * 1000
-    if n == 0
-    then return ()
-    else playNSteps (n - 1) delay $ boardNextState board
+  system "clear"
+  putStrLn $ showBoard board
+  threadDelay $ delay * 1000
+  if n == 0 then return () else playNSteps (n - 1) delay $ boardNextState board
